da5563d3403cf44a3233bc164eb743fa453c8b71
Theta Wang <thetawang@ThetadeMac-mini.local>
2019-06-12 18:44:03 +0800

    crawler

--- connectBigquery.py
+++ connectBigquery.py
@@ -2,6 +2,12 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
+def momoInsert(gid,picLink,title,base64,cat,itemLink):
+    sql = "INSERT INTO `howdress.pic.basic` (`picId`,`source_link`,`source_title`,`base64`,`description`, `detail_link` ) VALUES (\""+str(gid)+"\",\""+str(picLink)+"\",\""+str(title)+"\",\""+str(base64)+"\",\""+str(cat)+"\",\""+str(itemLink)+"\");"
+    #print(sql)
+    return sqlCommand(sql)
+
+
 def sqlCommand(sql):
     query_job = client.query(sql)  # API request
     table = query_job.result()  # Waits for query to finish
--- dockerfile
+++ dockerfile
@@ -1,6 +1,8 @@
 FROM gavin17423/forbigquery
 WORKDIR /app
 ADD . /app
+RUN ["pip3","install","BeautifulSoup4"]
+RUN ["pip3","install","selenium"]
 ENV GOOGLE_APPLICATION_CREDENTIALS="/app/HowDress-3b7e9bc86e83.json"
 
 CMD ["python3","/app/connectBigquery.py"]
